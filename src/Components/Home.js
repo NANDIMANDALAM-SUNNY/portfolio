7fe45594abdf7d5ed3136bee8a086dec80bd956c
NANDIMANDALAM SUNNY <90762658+NANDIMANDALAM-SUNNY@users.noreply.github.com>
2022-12-31 21:22:05 +0530

    Add files via upload

---
+++ src/Components/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import hieee from './Imgs/cofe.gif'
+import './Styles.css'
+const Home = () => {
+  return (
+    <>
+        <section className='container' id='home'>
+        <div className='typewriter'>
+                  <h2>Results Speak Louder than Words...</h2>
+                </div>
+            <div className='home'>
+              
+                {/* <img className='hieee' src={hieee} alt='...'/> */}
+            </div>
+        </section>
+    </>
+  )
+}
+
+export default Home
